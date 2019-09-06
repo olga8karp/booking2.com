0025bfce1aa743ab75148d46e87ab5ed45f8a62b
Olha Karpenko <o.karpenko@levi9.com>
2019-09-06 13:10:00 +0300

    implemented back and forth navigation for individual properties

--- src/app/components/property/property.component.ts
+++ src/app/components/property/property.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { DataStorageService } from 'src/app/services/data-storage.service';
 import { NgForm } from '@angular/forms';
 import { Property } from 'src/app/shared/property.model';
@@ -15,14 +15,20 @@ export class PropertyComponent implements OnInit {
   id: string;
   dateRange = {};
   isCalendarOpen = false;
+  goToPrevDisabled = false;
+  goToNextDisabled = false;
 
   constructor(private dataService: DataStorageService,
-              private router: Router, private route: ActivatedRoute,
-              private firestore: AngularFirestore) { }
+    private router: Router, private route: ActivatedRoute,
+    private firestore: AngularFirestore) { }
 
   ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id');
-    this.dataService.getPropertyById(this.id).subscribe((property: Property) => this.property = property);
+    this.route.paramMap.subscribe((routeParams: ParamMap) => {
+      this.id = routeParams.get('id');
+      this.dataService.getPropertyById(this.id).subscribe((prop: Property) => {
+        this.property = prop;
+      });
+    });
   }
 
   toggleCalendar(): void {
@@ -33,27 +39,41 @@ export class PropertyComponent implements OnInit {
     this.dataService.setBookedDates(this.id, form.value);
   }
 
-  goToPrevious(): void {
+  goToPrevious(id: number): void {
     this.firestore.collection('properties', ref => ref
-    .orderBy('timestamp', 'asc')
-    .startAfter(this.id)
-    .limit(1)
-  ).get()
-    .subscribe(response => {
-      console.log(response.docs);
-      // this.router.navigateByUrl('property');
-    });
+      .orderBy('timestamp', 'asc')
+      .startAfter(id)
+      .limit(2)
+    ).get()
+      .subscribe(response => {
+        this.goToNextDisabled = false;
+        if (!response.docs[1]) {
+          this.goToPrevDisabled = true;
+        }
+        if (response.docs[0]) {
+          this.router.navigateByUrl(`property/${response.docs[0].get('timestamp')}`);
+        } else {
+          this.goToPrevDisabled = true;
+        }
+      });
   }
 
-  goToNext(): void {
+  goToNext(id: number): void {
     this.firestore.collection('properties', ref => ref
-    .orderBy('timestamp', 'desc')
-    .startAfter(this.id)
-    .limit(2)
-  ).get()
-    .subscribe(response => {
-      console.log(response.docs[0].get('timestamp'));
-      this.router.navigateByUrl(`property/${response.docs[0].get('timestamp')}`);
-    });
+      .orderBy('timestamp', 'desc')
+      .startAfter(id)
+      .limit(2)
+    ).get()
+      .subscribe(response => {
+        this.goToPrevDisabled = false;
+        if (!response.docs[1]) {
+          this.goToNextDisabled = true;
+        }
+        if (response.docs[0]) {
+          this.router.navigateByUrl(`property/${response.docs[0].get('timestamp')}`);
+        } else {
+          this.goToNextDisabled = true;
+        }
+      });
   }
 }
