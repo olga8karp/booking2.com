9cebe2af265787ab165b48899b9f14c010fe49e0
Olha Karpenko <o.karpenko@levi9.com>
2019-09-26 16:23:47 +0300

    style adjustments

--- src/app/b2.module.ts
+++ src/app/b2.module.ts
@@ -1,5 +1,4 @@
 import { environment } from "src/environments/environment";
-
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { GooglePlaceModule } from "ngx-google-places-autocomplete";
@@ -23,20 +22,20 @@ import { PageNotFoundComponent } from "./components/page-not-found/page-not-foun
 import { SearchPanelComponent } from "./components/search-panel/search-panel.component";
 import { AddNewPropertyDetailsComponent } from "./components/add-new-property/add-new-property.component";
 import { DropzoneDirective } from "./directives/dropzone/dropzone.directive";
-import { UploaderComponent } from "./components/utility-components/images-uploader/uploader.component";
-import { UploadTaskComponent } from "./components/utility-components/images-uploader/upload-task/upload-task.component";
-import { AddressInputComponent } from "./components/utility-components/address-input/address-input.component";
-import { DatePickerComponent } from "./components/utility-components/date-picker/date-picker.component";
+import { UploaderComponent } from "./components/utility-components/data-input-components/images-uploader/uploader.component";
+import { UploadTaskComponent } from "./components/utility-components/data-input-components/images-uploader/upload-task/upload-task.component";
+import { AddressInputComponent } from "./components/utility-components/data-input-components/address-input/address-input.component";
+import { DatePickerComponent } from "./components/utility-components/data-input-components/date-picker/date-picker.component";
 import { LoginModalComponent } from "./components/utility-components/modals/login-modal/login-modal.component";
 import { BookingModalComponent } from "./components/utility-components/modals/booking-modal/booking-modal.component";
-import { NumberOfGuestsInputComponent } from "./components/utility-components/number-of-guests-input/number-of-guests-input.component";
-import { MealsInputComponent } from "./components/utility-components/meals-input/meals-input.component";
-import { FacilitiesInputComponent } from "./components/utility-components/facilities-input/facilities-input.component";
-import { RatingComponent } from "./components/utility-components/rating/rating.component";
-import { PropertyTypeInputComponent } from "./components/utility-components/property-type-input/property-type-input.component";
-import { RatingInputComponent } from "./components/utility-components/rating-input/rating-input.component";
-import { PriceRangeInputComponent } from "./components/utility-components/price-range-input/price-range-input.component";
-import { SearchTermInputComponent } from "./components/utility-components/search-term-input/search-term-input.component";
+import { NumberOfGuestsInputComponent } from "./components/utility-components/data-input-components/number-of-guests-input/number-of-guests-input.component";
+import { MealsInputComponent } from "./components/utility-components/data-input-components/meals-input/meals-input.component";
+import { FacilitiesInputComponent } from "./components/utility-components/data-input-components/facilities-input/facilities-input.component";
+import { RatingComponent } from "./components/utility-components/data-display-components/rating/rating.component";
+import { PropertyTypeInputComponent } from "./components/utility-components/data-input-components/property-type-input/property-type-input.component";
+import { RatingInputComponent } from "./components/utility-components/data-input-components/rating-input/rating-input.component";
+import { PriceRangeInputComponent } from "./components/utility-components/data-input-components/price-range-input/price-range-input.component";
+import { SearchTermInputComponent } from "./components/utility-components/data-input-components/search-term-input/search-term-input.component";
 
 
 @NgModule({
